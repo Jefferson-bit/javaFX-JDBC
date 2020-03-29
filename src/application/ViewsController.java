11b7fe317c52dc0cd166d6757351c10cc2bfb91a
jefferson-bit <jhonsonbabi@hotmail.com>
2020-03-29 19:26:39 -0300

    Main view controller

--- src/application/ViewsController.java
+++ src/application/ViewsController.java
@@ -7,15 +7,40 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 
 
 public class ViewsController implements Initializable {
 
-
+    @FXML
+    private MenuItem menuItemSeller;
+    
+    @FXML
+    private MenuItem menuItemDepartment;
+    
+    @FXML
+    private MenuItem menuItemAbout;
+    
+    @FXML
+    public void onMenuItemSellerAction(){
+            System.out.println("Hello Selller");
+    }
+    
+    @FXML
+    public void onMenuItemDepartmentAction(){
+            System.out.println("Hello Department");
+    }
+    @FXML
+    public void onMenuItemAboutAction(){
+            System.out.println("Hello About");
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+     
+        
     }    
     
 }
